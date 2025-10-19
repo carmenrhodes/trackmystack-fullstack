36c793005a3ae12391684358a69b51ee820e0feb
carmenrhodes <carmenrhodes289@gmail.com>
2025-10-19 11:48:33 -0500

    created new project with Spring Initializr

---
+++ java-spring-boot-back-end-app/src/main/java/com/trackmystack/backend/TrackMyStackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trackmystack.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMyStackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMyStackBackendApplication.class, args);
+	}
+
+}
---
+++ java-spring-boot-back-end-app/src/test/java/com/trackmystack/backend/TrackMyStackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackmystack.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMyStackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
